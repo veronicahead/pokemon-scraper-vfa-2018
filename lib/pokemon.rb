aaeb513ff8c4e004efcd18c558485474f6672225
Veronica Head <veronica.leigh.head@gmail.com>
2018-06-26 00:37:05 +0000

    automatically backed up by learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,7 +1,7 @@
 class Pokemon
   attr_accessor :id, :name, :type, :hp, :db 
   
-  def initialize (id:, name:, type:, db:)
+  def initialize (id:, name:, type:, hp: nil, db:)
   end 
   
   def save
